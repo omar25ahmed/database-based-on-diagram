771b2da5927002442764369d6017e7bfa8105787
behnam1369 <behnam.aghaali@yahoo.com>
2022-05-04 14:55:57 +0430

    Add relationships

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,5 +1,3 @@
-/* Database schema to keep the structure of entire database. */
-
 CREATE TABLE patients (
 	id int generated always as identity,
 	name varchar(250),
@@ -10,11 +8,10 @@ CREATE TABLE patients (
 CREATE TABLE medical_histories (
 	id int generated always as identity,
 	admitted_at timestamp,
-	patient_id int,
+	patient_id int REFERENCES patients (id),
 	status varchar(250),
 	PRIMARY KEY (id)
 );
-
 
 CREATE TABLE treatments (
 	id int generated always as identity,
@@ -27,7 +24,7 @@ CREATE TABLE invoices (
 	id int generated always as identity,
 	total_amount decimal,
 	generated_at timestamp,
-	medical_history_id int,
+	medical_history_id int REFERENCES medical_histories(id) ,
 	PRIMARY KEY (id)
 );
 
@@ -36,8 +33,14 @@ CREATE TABLE invoices_items (
 	unit_price decimal,
 	quantity int,
 	total_price decimal,
-	invoice_id int,
-	treatment_id int,
+	invoice_id int REFERENCES invoices (id),
+	treatment_id int REFERENCES treatments (id) ,
 	PRIMARY KEY (id)
 );
+
+CREATE TABLE medical_histories_treatments (
+	medical_history_id int REFERENCES medical_histories(id), 
+	treatment_id int REFERENCES treatments(id)
+);
+
 
